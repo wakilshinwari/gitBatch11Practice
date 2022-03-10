9d6e1b5f3948b12e868024194cc1cf571f84bf66
ShinwariWakil <shinwariabdulw@gmail.com>
2022-03-10 15:38:26 -0500

    3rd Commit

--- src/gitDemo01/Main.java
+++ src/gitDemo01/Main.java
@@ -3,5 +3,8 @@ package gitDemo01;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Batch11GitPractice project");
+
+        System.out.println("Batch12");
+
     }
 }
